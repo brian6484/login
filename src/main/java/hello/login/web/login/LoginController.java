9b2d24c861777d5e4ac1d3e1fb8b8dfeb1289797
Brian <brianlee6484@gmail.com>
2022-06-10 16:44:43 +0900

    Initial commit

---
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -0,0 +1,44 @@
+package hello.login.web.login;
+
+import hello.login.domain.login.LoginService;
+import hello.login.domain.member.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class LoginController {
+    private final LoginService loginService;
+
+    @GetMapping("/login")
+    public String loginForm(@ModelAttribute("loginForm") LoginForm loginForm){
+        return "login/loginForm";
+    }
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response){
+        if(bindingResult.hasErrors()){
+            return "login/loginForm";
+        }
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+        if(loginMember==null){
+            bindingResult.reject("loginFail","ur id / password is wrong");
+            return "login/loginForm";
+        }
+//        cookie
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
+
+        //when login success TODO
+        return "redirect:/";
+    }
+}
